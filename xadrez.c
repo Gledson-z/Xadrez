0c1300a89f37d0d7b54563bdba4a8b18f06a48bf
Gledson Souza <Gledsonsouza.dr@gmail.com>
2025-03-21 02:17:18 +0000

    Movimento do cavalo(L)

--- xadrez.c
+++ xadrez.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 int main()
 {
     // Move a torre 5 casas para a direita
-    printf("Torre move 5 casas para a direita.\n");
+    printf("\nTorre move 5 casas para a direita.\n");
 
     for (int T = 0; T < 5; T++)
     {
@@ -10,7 +11,7 @@ int main()
     }
 
     // Move o bispo 5 casas na diagonal para cima e à direita
-    printf("Bispo move 5 casas na diagonal para cima e à direita.\n");
+    printf("\nBispo move 5 casas na diagonal para cima e à direita.\n");
 
     int B = 0;
     while (B < 5)
@@ -20,7 +21,7 @@ int main()
     }
 
     // Move a rainha 8 casas para a esquerda
-    printf("Rainha move 8 casas para a esquerda.\n");
+    printf("\nRainha move 8 casas para a esquerda.\n");
 
     int R = 0;
     do
@@ -28,6 +29,19 @@ int main()
         printf("Esquerda\n");
         R++;
     } while (R < 8);
+
+    // Move o cavalo em L (move 2 casas em uma direção (horizontal ou vertical) e depois 1 casa perpendicularmente)
+    printf("\nCavalo move-se em L 2 casas para baixo 1 para a direita.\n");
+
+    int movimentoL = 1; // Quantidade de movimentos em L
+    while (movimentoL--)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Direita\n");
+    }
 
     return 0;
 }
